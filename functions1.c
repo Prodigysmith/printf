9c7e1958f8f9245b8bf14954be8eabc770322fdc
Elijah Arhinful <144293959+Prodigysmith@users.noreply.github.com>
2023-11-15 01:27:06 +0000

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -2,15 +2,21 @@
 
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
- * print_unsigned - Prints an unsigned number
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed.
+ * print_unsigned - Prints an unsigned number.
+ *
+ * @types: List of arguments representing data types.
+ * @buffer: Buffer array used for handling the print operation.
+ * @flags: Flags used to calculate formatting options.
+ * @width: Width parameter for printing.
+ * @precision: Precision specification for printing.
+ * @size: Size specifier for the unsigned number.
+ *
+ * Return: The number of characters printed.
  */
+int print_unsigned(/* parameters */) {
+    // Function implementation goes here
+}
+
 int print_unsigned(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
